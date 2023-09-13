2c4bfa771cb700f6a0f617da92347b47e27805ed
Jiyan Park <55106069+JiyanPark@users.noreply.github.com>
2023-09-13 13:56:12 +0900

    Add files via upload

---
+++ jiyanhw1.py
@@ -0,0 +1,4 @@
+name = "Jiyan Pak"  # Replace "Your Name" with your actual name
+student_id = "202212205"  # Replace "XXXXXXXX" with your actual student ID
+
+print(f"My name is {name} and student ID is {student_id}")
